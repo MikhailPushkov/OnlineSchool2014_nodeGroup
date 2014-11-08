501729dc4be224f4adf01390709f3bb34da7aebe
ДмитрийЗолотов <vareniedimmka@mail.ru>
2014-11-08 14:17:06 +0300

    добавил изменения в route_teacher

--- app/routes/route_teacher.js
+++ app/routes/route_teacher.js
@@ -3,21 +3,37 @@ var Teacher = require('../models/shema_teacher');
 var fs = require("fs");
 var _ = require('underscore');
 
-
-
-exports.Teacher_findById = function(req, res) {
+exports.Teacher_findById = function (req, res) {
     var id = req.params.id;
-    Teacher.findOne({ _id: id }).populate('user').exec(function(err, image) {
-        if (err) return res.send(404,"Teacher not found");
-         res.send(image);
+    console.log(id);
+    Teacher.findOne({ _id: id }, function (err, Teacher) {
+        if (err) return res.send(404, "Teacher not found");
+        res.send(Teacher);
     });
 };
 
-exports.findAll = function(req, res) {
-    var teacher_id = req.query.parent_id;
-    Teacher.find({'parent': parent_id}).populate('user').exec(function(err, teacher_collection) {
-        if (err) return console.error(err);
-         res.send(teacher_collection);
+
+exports.Teacher_findAll = function (req, res) {
+    var _id = req.query.user_id;
+    var my_array = [];
+
+    if (_id) {
+        Teacher.find({ '_id': { $ne: _id } }, function (err, collection) {
+            _.each(collection, function (friend) {
+                friend.local = "";
+                my_array.push(friend._id.toString());
+            });
+            res.send(collection);
+        });
+        return;
+    }
+
+    Teacher.find({}, function (err, collection) {
+        _.each(collection, function (friend) {
+            friend.local = "";
+            my_array.push(friend._id.toString());
+        });
+        res.send(collection);
     });
 };
 
@@ -30,14 +46,14 @@ exports.addTeacher = function(req, res) {
     });
 };
 
-exports.updateParent = function(req, res) {
+exports.updateTeacher = function(req, res) {
     var _id = req.params.id;
-    var parent = req.body;
+    var teacher = req.body;
     delete teacher._id;  /// I need to remove this here, I need to figure out why this is happening, must be a MongoDB configuration
-        Teacher.findOneAndUpdate({'_id':_id}, parent, {safe:true}, function(err, result) {
+        Teacher.findOneAndUpdate({'_id':_id}, teacher, {safe:true}, function(err, result) {
             if (err) {
-                console.log('Error updating parent: ' + err);
-                res.send(500,{'error':'An error updating parent'});
+                console.log('Error updating teacher: ' + err);
+                res.send(500,{'error':'An error updating teacher'});
             } else {
                 res.send(teacher);
             }
