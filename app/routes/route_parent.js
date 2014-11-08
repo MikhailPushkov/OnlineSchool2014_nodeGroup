d4ac7f8d7ba1082a9fad278d6ff1ab6ee6fdbb0f
ДмитрийЗолотов <vareniedimmka@mail.ru>
2014-11-08 14:16:46 +0300

    Добавил изменения в route_parent

--- app/routes/route_parent.js
+++ app/routes/route_parent.js
@@ -6,26 +6,43 @@ var _ = require('underscore');
 
 
 
-exports.Parent_findById = function(req, res) {
+exports.Parent_findById = function (req, res) {
     var id = req.params.id;
-    Image.findOne({ _id: id }).populate('user').exec(function(err, image) {
-        if (err) return res.send(404,"Parent not found");
-         res.send(image);
+    console.log(id);
+    Parent.findOne({ _id: id }, function (err, Parent) {
+        if (err) return res.send(404, "Parent not found");
+        res.send(Parent);
     });
 };
 
-exports.findAll = function(req, res) {
-    var parent_id = req.query.parent_id;
-    Image.find({'parent': parent_id}).populate('user').exec(function(err, image_collection) {
-        if (err) return console.error(err);
-         res.send(image_collection);
+exports.Parent_findAll = function (req, res) {
+    var _id = req.query.user_id;
+    var my_array = [];
+
+    if (_id) {
+        Parent.find({ '_id': { $ne: _id } }, function (err, collection) {
+            _.each(collection, function (friend) {
+                friend.local = "";
+                my_array.push(friend._id.toString());
+            });
+            res.send(collection);
+        });
+        return;
+    }
+
+    Parent.find({}, function (err, collection) {
+        _.each(collection, function (friend) {
+            friend.local = "";
+            my_array.push(friend._id.toString());
+        });
+        res.send(collection);
     });
 };
 
 exports.addParent = function(req, res) {
     var parent = req.body;
     var Parent_model = new Parent(parent);
-    Parent_model.save(function (err, new_image) {
+    Parent_model.save(function (err, new_parent) {
       if (err) return console.error(err);
       res.send(new_parent);
     });
@@ -35,7 +52,7 @@ exports.updateParent = function(req, res) {
     var _id = req.params.id;
     var parent = req.body;
     delete parent._id;  /// I need to remove this here, I need to figure out why this is happening, must be a MongoDB configuration
-        Image.findOneAndUpdate({'_id':_id}, parent, {safe:true}, function(err, result) {
+        Parent.findOneAndUpdate({'_id':_id}, parent, {safe:true}, function(err, result) {
             if (err) {
                 console.log('Error updating parent: ' + err);
                 res.send(500,{'error':'An error updating parent'});
@@ -48,7 +65,7 @@ exports.updateParent = function(req, res) {
 exports.deleteParent = function (req, res) {
     var id = req.params.id;
 
-    User.findOneAndRemove({'_id': id }, {safe: true}, function (err, result) {
+    Parent.findOneAndRemove({'_id': id }, {safe: t45rue}, function (err, result) {
         if (err) {
             res.send({'error': 'An error has occurred - ' + err});
         } else {
