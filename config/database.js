94351169eae87007a0a41766f7b91229206ff7bf
sergey vlasov <qlir@ya.ru>
2014-11-19 00:10:02 +0300

    добавлены настройки для монгоДБ

--- config/database.js
+++ config/database.js
@@ -1,5 +1,5 @@
 // config/database.js
 module.exports = {
-	 'url' : 'mongodb://localhost/elecscool' // looks like mongodb://<user>:<pass>@mongo.onmodulus.net:27017/Mikha4ot
-    //'url'  :  'mongodb://dimkaipx:159357b@ds051980.mongolab.com:51980/heroku_app31690046'
+	 //'url' : 'mongodb://localhost/elecscool' // looks like mongodb://<user>:<pass>@mongo.onmodulus.net:27017/Mikha4ot
+    'url'  :  'mongodb://dimkaipx:159357b@ds051980.mongolab.com:51980/heroku_app31690046'
 };
