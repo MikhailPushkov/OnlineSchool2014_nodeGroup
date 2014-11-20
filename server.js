e2dd70c2f0bf2586bfdebfd8580e769a67879270
sergey <serge@sergey.ser>
2014-11-20 23:59:17 +0300

    lkjfdldfljf

--- server.js
+++ server.js
@@ -9,5 +9,5 @@ var passport = require('passport');
 var flash    = require('connect-flash');
 var configDB = require('./config/database.js');
 
-mongoose.connect("mongodb://heroku_app31763119:31nopdab5dgq7c934i0hvksivs@ds053080.mongolab.com:53080/heroku_app31763119");
+mongoose.connect("mongodb://dimkaipx91:159357bbB@ds053080.mongolab.com:53080/heroku_app31763119");
 console.log("OOOOOOOOOOOOOOOOOOOOOOOOOOOOKKKKKKKKKKKKKKKKKKKKKKKKKK");
