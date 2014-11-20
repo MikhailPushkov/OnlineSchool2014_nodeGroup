4c8ad9c21c06eab15d51d211b8145df7b04bb37f
sergey <serge@sergey.ser>
2014-11-21 00:35:10 +0300

    first deploy

--- config/database.js
+++ config/database.js
@@ -1,5 +1,5 @@
 // config/database.js
 module.exports = {
 	 //'url' : 'mongodb://localhost/elecscool' // looks like mongodb://<user>:<pass>@mongo.onmodulus.net:27017/Mikha4ot
-    'url'  :  'mongodb://heroku_app31763119:31nopdab5dgq7c934i0hvksivs@ds053080.mongolab.com:53080/heroku_app31763119'
+    'url'  :  'mongodb://admin:admin@ds053080.mongolab.com:53080/heroku_app31763119'
 };
--- server.js
+++ server.js
@@ -1,13 +1,43 @@
-console.log("!!!!!!!!!!!!!!!");
-var express  = require('express');
+var express = require('express');
 
 var MongoStore = require('connect-mongo')(express);
-var app      = express();
-var port     = process.env.PORT || 8080;
+var app = express();
+var port = process.env.PORT || 8080;
 var mongoose = require('mongoose');
 var passport = require('passport');
-var flash    = require('connect-flash');
+var flash = require('connect-flash');
 var configDB = require('./config/database.js');
+mongoose.connect(configDB.url);
+var sessionOpts = {
+    key: "token",
+    secret: 'ilovescotchscotchyscotchscotch',
+    cookie: { maxAge: 30 * 60 * 1000 },
+    store: new MongoStore({
+        db: mongoose.connection.db
+    })
+};
 
-mongoose.connect("mongodb://admin:admin@ds053080.mongolab.com:53080/heroku_app31763119");
-console.log("OOOOOOOOOOOOOOOOOOOOOOOOOOOOKKKKKKKKKKKKKKKKKKKKKKKKKK");
+app.set('root_directory', __dirname);
+
+require('./config/passport')(passport, app); // pass passport for configuration
+
+app.configure(function () {
+
+    // set up our express application
+    app.use(express.logger('dev')); // log every request to the console
+    app.use(express.cookieParser()); // read cookies (needed for auth)
+    app.use(express.bodyParser()); // get information from html forms
+    app.use(express.static(__dirname + '/public'));
+    app.set('view engine', 'ejs'); // set up ejs for templating
+
+    // required for passport
+    app.use(express.session(sessionOpts)); // session secret
+    app.use(passport.initialize());
+    app.use(passport.session()); // persistent login sessions
+    app.use(flash()); // use connect-flash for flash messages stored in session
+
+});
+
+require('./app/routes.js')(app, passport); // load our routes and pass in our app and fully configured passport
+
+var server = app.listen(port);
