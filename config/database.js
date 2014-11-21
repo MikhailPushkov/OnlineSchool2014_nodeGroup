b9b9752dbfdac1de6401a019f516b4b9a9354b0a
SergeyVlasov <qlir@ya.ru>
2014-11-22 00:38:00 +0300

    changes db settings for developers

--- config/database.js
+++ config/database.js
@@ -1,5 +1,5 @@
 // config/database.js
 module.exports = {
-	 //'url' : 'mongodb://localhost/elecscool' // looks like mongodb://<user>:<pass>@mongo.onmodulus.net:27017/Mikha4ot
-    'url'  :  'mongodb://admin:admin@ds053080.mongolab.com:53080/heroku_app31763119'
+	'url' : 'mongodb://localhost/elecscool' // looks like mongodb://<user>:<pass>@mongo.onmodulus.net:27017/Mikha4ot
+    //'url'  :  'mongodb://admin:admin@ds053080.mongolab.com:53080/heroku_app31763119'
 };
